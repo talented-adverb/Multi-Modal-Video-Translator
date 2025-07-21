5891bf7012b84a0377be403403d421bfcb25b1bb
pratham-2004 <ps.pratham04@gmail.com>
2025-07-21 23:24:29 +0530

    video_translation_v1

---
+++ VideoTranslation__Integration_DEMO/tesseract.py
@@ -0,0 +1,4 @@
+import pytesseract
+# Optionally set the command if needed:
+# pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+print(pytesseract.get_tesseract_version())
